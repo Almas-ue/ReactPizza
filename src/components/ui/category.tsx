2d3d1638c168c0618bb92fb14a1398cf95919627
Almas-ue <almas200224@gmail.com>
2025-01-31 18:59:44 +0500

    change ui category

--- src/components/ui/category.tsx
+++ src/components/ui/category.tsx
@@ -1,19 +1,21 @@
-import { FC } from "react";
+import { cn } from "@/lib/utils";
+import { FC, useState } from "react";
 
 interface CategoryName {
     categoryList: string[];
 }
 
 const Category: FC<CategoryName> = ({ categoryList }) => {
+    const [isActive, setActive] = useState<string>("Все");
     return (
         <div className="inline-flex gap-1 p-[5px] rounded-2xl bg-[#F5F5F5]">
             {categoryList && categoryList.map((name, index) => (
                 <>
-                    <button key={index + 1} className="px-6 py-3 rounded-2xl bg-white text-[#FE5F00]">{name}</button>
+                    <button key={index} onClick={() => setActive(name)} className={cn("px-6 py-3 rounded-2xl", isActive === name ? "bg-white text-[#FE5F00]" : "")}>{name}</button >
                 </>
             ))
             }
-        </div>
+        </div >
 
     )
 }
